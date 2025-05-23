8a5566ee6bed50817f79b32916dbc46943fa4929
NB-NICHOLASR\NicholasRicci <nicholas.ricci@3uvision.com>
2025-05-23 14:17:02 +0200

    Finish automatic backup

---
+++ Onova/Services/IMultiProgressBar.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace Onova.Services
+{
+    public interface IMultiProgressBar
+    {
+        IProgress<double> CreateProgressBar(string name, string description = "");
+
+        void CreateGlobalProgressBar();
+    }
+}
